13710d1429ddf78099d71a5da66085726d9bf2a8
vkletsko <v.kletsko@gmail.com>
2024-01-23 12:18:55 +0200

    update datasets, readme

--- README.md
+++ README.md
@@ -4,3 +4,21 @@
 #### Master of degree from university, task
 
 Порівняння різних алгоритмів сортування
+
+Input data sets
+Для тестування було взято набори даних у вигляді масивів із 1000 чисел та 1000 слів різної довжини
+```
+Sorting results
+
+insertion sort 100 time: 0.019738 sec
+merge sort  100 time:    0.012170 sec
+built-in sorted() 100 time: 0.000470 sec
+built-in sort() 100 time: 0.000418 sec
+
+Sorting results
+
+insertion sort 10_000 time: 176.560238 sec
+merge sort  10_000 time: 1.889510 sec
+built-in sorted() 10_000 time: 0.114519 sec
+built-in sort() 10_000 time: 0.114135 sec
+```
--- src/task.py
+++ src/task.py
@@ -47,29 +47,41 @@ def merge(left, right):
     return merged
 
 
-variaty = 100  # Can be changed
-list_len = 1000  # Can be changed
+def calculate_time_and_print(list_to_sort, elems_count):
+    insertion_sort = timeit.timeit(
+        f"insertion_sort({list_to_sort}.copy())", globals=globals(), number=100
+    )
+    merge_sort = timeit.timeit(
+        f"merge_sort({list_to_sort}.copy())", globals=globals(), number=100
+    )
+    timsort_one = timeit.timeit(
+        f"sorted({list_to_sort}.copy())", globals=globals(), number=100
+    )
+    timsort_two = timeit.timeit(
+        f"{list_to_sort}.copy().sort()", globals=globals(), number=100
+    )
 
-arr_to_sort = [random.randint(1, variaty) for _ in range(list_len)]
+    print("\nSorting results\n")
+    print(
+        "{:<24}".format(f"insertion sort {elems_count} time:"),
+        f"{insertion_sort:.6f} sec",
+    )
+    print("{:<24}".format(f"merge sort  {elems_count} time:"), f"{merge_sort:.6f} sec")
+    print("{:<24}".format(f"built-in sorted() {elems_count} time:"), f"{timsort_one:.6f} sec")
+    print("{:<24}".format(f"built-in sort() {elems_count} time:"), f"{timsort_two:.6f} sec")
 
-insertion_sort = timeit.timeit(
-    "insertion_sort(arr_to_sort.copy())", globals=globals(), number=100
-)
-merge_sort = timeit.timeit(
-    "merge_sort(arr_to_sort.copy())", globals=globals(), number=100
-)
-timsort_one = timeit.timeit(
-    "sorted(arr_to_sort.copy())", globals=globals(), number=100
-)
-timsort_two = timeit.timeit(
-    "arr_to_sort.copy().sort()", globals=globals(), number=100
-)
+variaty = 1000  # Can be changed
+list_len1 = 100
+list_len2 = 10000
 
-print("\nSorting results\n")
-print(
-    "{:<24}".format("insertion sort time:"),
-    f"{insertion_sort:.6f} sec",
-)
-print("{:<24}".format("merge sort time:"), f"{merge_sort:.6f} sec")
-print("{:<24}".format("built-in sorted() time:"), f"{timsort_one:.6f} sec")
-print("{:<24}".format("built-in sort() time:"), f"{timsort_two:.6f} sec")
+arr_to_sort_100 = [random.randint(1, variaty) for _ in range(list_len1)]
+arr_to_sort_10_000 = [random.randint(1, variaty) for _ in range(list_len2)]
+
+compare_arrays = [
+    ("arr_to_sort_100", "100"), 
+    ("arr_to_sort_10_000", "10_000")
+]
+
+for (list_to_sort, elems_count)  in compare_arrays:
+    calculate_time_and_print(list_to_sort, elems_count)
+
